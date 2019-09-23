49ae04691aef55e5320ef60e7c38deb15bc7a624
Silap Aliyev <sylapaliyev@gmail.com>
2019-09-23 12:46:57 +0900

    Fixed typo in stress test

--- tests/stress-10000-index-html.sh
+++ tests/stress-10000-index-html.sh
@@ -3,5 +3,5 @@
 
 mkdir trash
 pushd trash
-eq 1 10000 | xargs -n1 -P10000 bash -c 'i=$0; url="http://localhost:8080/index.html"; filename="index-${i}.html"; curl -s --http1.1 -o $filename $url'
+seq 1 10000 | xargs -n1 -P10000 bash -c 'i=$0; url="http://localhost:8080/index.html"; filename="index-${i}.html"; curl -s --http1.1 -o $filename $url'
 popd
